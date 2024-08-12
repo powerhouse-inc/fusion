3e8e64f1f32ee18b7ebffb2c3ae3af06c581a907
Yasiel Cabrera <yasiel9506@gmail.com>
2024-08-12 09:58:39 -0400

    fix: remove SSR forum preload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { SWRConfig, unstable_serialize as unstableSerialize } from 'swr';
 import type { ExtendedExecutiveProposal } from '@/core/models/interfaces/makervote';
 import type { Roadmap } from '@/core/models/interfaces/roadmaps';
 import type { Team } from '@/core/models/interfaces/team';
@@ -6,11 +5,9 @@ import { fetchActors } from '@/views/EcosystemActorsIndex/api/queries';
 import HomeView from '@/views/Home/HomeView';
 import type { FormattedFinancesData } from '@/views/Home/api/finances';
 import { getFinancesData } from '@/views/Home/api/finances';
-import { getForumPosts } from '@/views/Home/api/forum';
 import { getGovernanceProposals } from '@/views/Home/api/makervoteQueries';
 import type { RevenueAndSpendingRecords } from '@/views/Home/api/revenueAndSpending';
 import { getRevenueAndSpendingData } from '@/views/Home/api/revenueAndSpending';
-import { ForumCategories } from '@/views/Home/components/GovernanceSection/ForumOverview/categories';
 import { getScopeOfWorkState } from '@/views/RoadmapMilestones/api/queries';
 import type { GetServerSideProps, NextPage } from 'next';
 
@@ -20,7 +17,6 @@ interface HomePageProps {
   teams: Team[];
   governanceProposals: ExtendedExecutiveProposal[];
   roadmaps: Roadmap[];
-  fallback: Record<string, unknown>;
 }
 
 const HomePage: NextPage<HomePageProps> = ({
@@ -29,28 +25,24 @@ const HomePage: NextPage<HomePageProps> = ({
   teams,
   governanceProposals,
   roadmaps,
-  fallback,
 }) => (
-  <SWRConfig value={{ fallback }}>
-    <HomeView
-      revenueAndSpendingData={revenueAndSpendingData}
-      financesData={financesData}
-      teams={teams}
-      governanceProposals={governanceProposals}
-      roadmaps={roadmaps}
-    />
-  </SWRConfig>
+  <HomeView
+    revenueAndSpendingData={revenueAndSpendingData}
+    financesData={financesData}
+    teams={teams}
+    governanceProposals={governanceProposals}
+    roadmaps={roadmaps}
+  />
 );
 
 export default HomePage;
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const [revenueAndSpendingData, financesData, teams, governanceProposals, forumPosts, roadmaps] = await Promise.all([
+  const [revenueAndSpendingData, financesData, teams, governanceProposals, roadmaps] = await Promise.all([
     getRevenueAndSpendingData(),
     getFinancesData(),
     fetchActors(),
     getGovernanceProposals(),
-    getForumPosts(ForumCategories[0].id),
     getScopeOfWorkState(),
   ]);
 
@@ -61,9 +53,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
       teams,
       governanceProposals,
       roadmaps,
-      fallback: {
-        [unstableSerialize(['forum', ForumCategories[0].id.toString()])]: forumPosts,
-      },
     },
   };
 };
