6f5390bd8d98282f7cbd7ae18a6e27b4504501c1
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2024-08-22 23:37:24 -0300

    📝 Reskin Finances: Breakdown Chart
    🎟️ Ticket
    🔗 Trello Card - https://trello.com/c/6H8x1mXD/486-reskin-finances-breakdown-chart
    
    🛠️ What This PR Does
    🟢 Feature: Should update the loading state for light/dark mode. Desktop/Small resolutions.
    🖥️ Resolution: Optimized the layout and design for better display on desktop resolutions.
    🚀 Improvements & Fixes
    Add the new loading with the reskin styles

--- src/views/Finances/components/BreakdownChartSection/BreakdownChart/BreakdownChartSkeletonUtils/BarSkeletonItem.tsx
+++ src/views/Finances/components/BreakdownChartSection/BreakdownChart/BreakdownChartSkeletonUtils/BarSkeletonItem.tsx
@@ -4,101 +4,127 @@ import React from 'react';
 const mobileBarsValues = [
   {
     height: 148,
-    percentFilled: 148,
   },
   {
     height: 150,
-    percentFilled: 150,
   },
   {
     height: 155,
-    percentFilled: 155,
   },
   {
     height: 139,
-    percentFilled: 139,
   },
   {
     height: 150,
-    percentFilled: 98,
   },
   {
     height: 156,
-    percentFilled: 82,
   },
   {
     height: 162,
-    percentFilled: 62,
   },
   {
     height: 166,
-    percentFilled: 45,
   },
   {
     height: 164,
-    percentFilled: 36,
+    heightRelative: 13,
   },
   {
     height: 162,
-    percentFilled: 23,
+    heightRelative: 23,
   },
   {
     height: 169,
-    percentFilled: 15,
+    heightRelative: 53,
   },
   {
     height: 168,
-    percentFilled: 7,
+    heightRelative: 48,
   },
 ];
 const tabletBarsValues = [
   {
+    height: 195,
+  },
+  {
+    height: 195,
+  },
+  {
+    height: 196,
+  },
+  {
+    height: 180,
+  },
+  {
+    height: 185,
+  },
+  {
+    height: 192,
+  },
+  {
+    height: 197,
+  },
+  {
+    height: 208,
+  },
+  {
+    height: 211,
+    heightRelative: 13,
+  },
+  {
+    height: 215,
+    heightRelative: 23,
+  },
+  {
+    height: 220,
+    heightRelative: 53,
+  },
+  {
+    height: 228,
+    heightRelative: 48,
+  },
+];
+const DeskBarsValues = [
+  {
+    height: 245,
+  },
+  {
+    height: 244,
+  },
+  {
+    height: 246,
+  },
+  {
+    height: 226,
+  },
+  {
+    height: 256,
+  },
+  {
+    height: 264,
+  },
+  {
+    height: 273,
+  },
+  {
+    height: 286,
+  },
+  {
+    height: 291,
+    heightRelative: 13,
+  },
+  {
     height: 297,
-    percentFilled: 297,
+    heightRelative: 23,
   },
   {
-    height: 296,
-    percentFilled: 296,
+    height: 303,
+    heightRelative: 53,
   },
   {
-    height: 299,
-    percentFilled: 299,
-  },
-  {
-    height: 274,
-    percentFilled: 274,
-  },
-  {
-    height: 296,
-    percentFilled: 192,
-  },
-  {
-    height: 306,
-    percentFilled: 160,
-  },
-  {
-    height: 316,
-    percentFilled: 124,
-  },
-  {
-    height: 332,
-    percentFilled: 101,
-  },
-  {
-    height: 338,
-    percentFilled: 70,
-  },
-  {
-    height: 345,
-    percentFilled: 50,
-  },
-  {
-    height: 353,
-    percentFilled: 30,
-  },
-  {
-    height: 376,
-    percentFilled: 16,
+    height: 308,
+    heightRelative: 48,
   },
 ];
 
@@ -107,17 +133,24 @@ const BarSkeletonItems: React.FC = () => (
     <Mobile>
       {mobileBarsValues.map((item, index) => (
         <WrapperBox height={item.height} key={index}>
-          <InsideBox height={item.percentFilled} />
+          {item.heightRelative && <InsideBox height={item.heightRelative} />}
         </WrapperBox>
       ))}
     </Mobile>
     <Table>
       {tabletBarsValues.map((item, index) => (
         <WrapperBox height={item.height} key={index}>
-          <InsideBox height={item.percentFilled} />
+          {item.heightRelative && <InsideBox height={item.heightRelative} />}
         </WrapperBox>
       ))}
     </Table>
+    <Desk>
+      {DeskBarsValues.map((item, index) => (
+        <WrapperBox height={item.height} key={index}>
+          {item.heightRelative && <InsideBox height={item.heightRelative} />}
+        </WrapperBox>
+      ))}
+    </Desk>
   </ContainerBars>
 );
 export default BarSkeletonItems;
@@ -125,66 +158,45 @@ export default BarSkeletonItems;
 const ContainerBars = styled('div')(({ theme }) => ({
   display: 'flex',
   flexDirection: 'row',
-  alignItems: 'flex-end',
-  gap: 10,
+  gap: 8,
+  width: '100%',
   [theme.breakpoints.up('tablet_768')]: {
-    gap: 13,
-  },
-  [theme.breakpoints.up('desktop_1024')]: {
-    gap: 16,
+    width: '100%',
   },
   [theme.breakpoints.up('desktop_1280')]: {
     gap: 24,
   },
 }));
-const WrapperBox = styled('div')<{ height: number }>(({ theme, height }) => ({
+const WrapperBox = styled('div')<{ height: number; hasBorder?: boolean }>(({ theme, height }) => ({
   display: 'flex',
   position: 'relative',
   flexDirection: 'column',
   justifyContent: 'flex-end',
-  backgroundColor: theme.palette.mode === 'light' ? '#ECF1F3' : '#31424E',
+  alignItems: 'flex-end',
+  background: theme.palette.isLight ? theme.palette.colors.gray[100] : theme.palette.colors.slate[400],
   width: 16,
-  borderBottomLeftRadius: 4,
-  borderBottomRightRadius: 4,
-  borderTopLeftRadius: 4,
-  borderTopRightRadius: 4,
+  borderRadius: 4,
   overflow: 'hidden',
   height,
-
   [theme.breakpoints.up('tablet_768')]: {
-    width: 40,
-    borderBottomLeftRadius: 6,
-    borderBottomRightRadius: 6,
-    borderTopLeftRadius: 6,
-    borderTopRightRadius: 6,
+    width: 23,
+  },
+  [theme.breakpoints.up('desktop_1024')]: {
+    width: 30,
   },
   [theme.breakpoints.up('desktop_1280')]: {
-    width: 56,
-  },
-}));
-
-const InsideBox = styled('div')<{ height: number }>(({ theme, height }) => ({
-  backgroundColor: theme.palette.mode === 'light' ? '#D1DEE6' : '#47616f80',
-  width: 16,
-  borderBottomLeftRadius: 4,
-  borderBottomRightRadius: 4,
-  position: 'absolute',
-  height,
-  [theme.breakpoints.up('tablet_768')]: {
     width: 40,
-    borderBottomLeftRadius: 6,
-    borderBottomRightRadius: 6,
-  },
-  [theme.breakpoints.up('desktop_1280')]: {
-    width: 56,
   },
 }));
 
 const Mobile = styled('div')(({ theme }) => ({
   display: 'flex',
   flexDirection: 'row',
+  width: '100%',
+  gap: 8,
   alignItems: 'flex-end',
-  gap: 10,
+
+  justifyContent: 'space-between',
   [theme.breakpoints.up('tablet_768')]: {
     display: 'none',
   },
@@ -192,23 +204,63 @@ const Mobile = styled('div')(({ theme }) => ({
 
 const Table = styled('div')(({ theme }) => ({
   display: 'none',
+
   [theme.breakpoints.between('tablet_768', 'desktop_1024')]: {
     display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between',
+    selfAlign: 'end',
+    marginBottom: 8,
     flexDirection: 'row',
     alignItems: 'flex-end',
-    gap: 13,
+    gap: 4,
   },
 
   [theme.breakpoints.between('desktop_1024', 'desktop_1280')]: {
     display: 'flex',
+    width: '100%',
     flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'flex-end',
-    gap: 16,
+    gap: 8,
   },
+}));
+
+const Desk = styled('div')(({ theme }) => ({
+  display: 'none',
+
   [theme.breakpoints.up('desktop_1280')]: {
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'flex-end',
+    gap: 19,
+  },
+  [theme.breakpoints.up('desktop_1440')]: {
+    display: 'flex',
+
+    flexDirection: 'row',
+    alignItems: 'flex-end',
     gap: 24,
+  },
+}));
+
+const InsideBox = styled('div')<{ height?: number }>(({ theme, height }) => ({
+  backgroundColor: theme.palette.isLight ? theme.palette.colors.gray[300] : theme.palette.colors.slate[300],
+  display: 'flex',
+  top: 0,
+
+  width: 16,
+  position: 'absolute',
+  height,
+  [theme.breakpoints.up('tablet_768')]: {
+    width: 23,
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+  },
+  [theme.breakpoints.up('desktop_1024')]: {
+    width: 30,
+  },
+  [theme.breakpoints.up('desktop_1280')]: {
+    width: 40,
   },
 }));
