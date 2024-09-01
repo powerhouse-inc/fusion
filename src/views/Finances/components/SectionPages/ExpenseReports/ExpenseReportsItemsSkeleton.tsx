8791582f6f81838660b41ca86a6219f30a7e9e85
Nestor <nestor.diaz@dspot.com.pl>
2024-09-01 20:22:47 +0100

    feat: Reskin Finances: Budget Statements - Should update the loading state for light/dark mode. Desktop/Small resolutions.

--- src/views/Finances/components/SectionPages/ExpenseReports/ExpenseReportsItemsSkeleton.tsx
+++ src/views/Finances/components/SectionPages/ExpenseReports/ExpenseReportsItemsSkeleton.tsx
@@ -1,10 +1,10 @@
 import { styled } from '@mui/material';
-import DelegateExpenseTrendItemSkeleton from '../../DelegateExpenseTrend/DelegateExpenseTrendItemSkeleton';
+import DelegateExpenseTrendItemSkeleton from '@/views/Finances/components/DelegateExpenseTrend/DelegateExpenseTrendItemSkeleton';
 
 const ExpenseReportsItemsSkeleton = () => (
   <Container>
     {Array.from({ length: 10 }).map((_, index) => (
-      <DelegateExpenseTrendItemSkeleton key={index} />
+      <DelegateExpenseTrendItemSkeleton key={`item-skeleton-${index}`} />
     ))}
   </Container>
 );
@@ -14,9 +14,9 @@ export default ExpenseReportsItemsSkeleton;
 const Container = styled('div')(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
-  gap: 8,
+  gap: 16,
 
-  [theme.breakpoints.up('tablet_768')]: {
-    gap: 16,
+  [theme.breakpoints.up('desktop_1024')]: {
+    gap: 8,
   },
 }));
