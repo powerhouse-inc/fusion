02d1c85fed8600002803f89e3fb90d3a38999c42
Nestor <nestor.diaz@dspot.com.pl>
2024-09-04 12:47:03 +0100

    feat: RESKIN Single Roadmap Page - Should change the breadcrumb All resolutions - Should update the title/subtitle styles - Should update the section title styles - Should update the Roadmap card styles - Should update the Roadmap Card header styles - Should update the Roadmap card description styles - Should update the Roadmap card progress styles

--- src/views/RoadmapMilestones/components/sections/OverviewSection/OverviewSection.tsx
+++ src/views/RoadmapMilestones/components/sections/OverviewSection/OverviewSection.tsx
@@ -1,15 +1,15 @@
 import { styled } from '@mui/material';
-import React from 'react';
 import type { Milestone } from '@/core/models/interfaces/roadmaps';
 import RoadmapTimeline from '../../RoadmapTimeline/RoadmapTimeline';
 import SectionTitle from '../../SectionTitle/SectionTitle';
+import type { FC } from 'react';
 
 interface OverviewSectionProps {
   title: string;
   milestones: Milestone[];
 }
 
-const OverviewSection: React.FC<OverviewSectionProps> = ({ title, milestones }) => (
+const OverviewSection: FC<OverviewSectionProps> = ({ title, milestones }) => (
   <Section>
     <SectionTitle
       title={title}
@@ -24,18 +24,10 @@ const OverviewSection: React.FC<OverviewSectionProps> = ({ title, milestones }) 
 
 export default OverviewSection;
 
-const Section = styled('section')(({ theme }) => ({
-  marginTop: 16,
-
-  [theme.breakpoints.up('tablet_768')]: {
-    marginTop: 32,
-  },
+const Section = styled('section')(() => ({
+  marginTop: 24,
 }));
 
-const TimelineContainer = styled('div')(({ theme }) => ({
-  marginTop: 18,
-
-  [theme.breakpoints.up('tablet_768')]: {
-    marginTop: 40,
-  },
+const TimelineContainer = styled('div')(() => ({
+  marginTop: 24,
 }));
