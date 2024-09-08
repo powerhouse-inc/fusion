f30c41282ee471c8a2e9b57741dd67ea781fdacd
Yasiel Cabrera <yasiel9506@gmail.com>
2024-09-08 15:16:47 -0400

    fix: milestone link colors

--- src/views/ActorProjects/components/MilestoneLink/MilestoneLink.tsx
+++ src/views/ActorProjects/components/MilestoneLink/MilestoneLink.tsx
@@ -1,28 +1,24 @@
-import { styled, useTheme } from '@mui/material';
-import ArrowNavigationForCards from '@ses/components/svg/ArrowNavigationForCards';
+import { styled } from '@mui/material';
 import { siteRoutes } from '@ses/config/routes';
 import Link from 'next/link';
+import ArrowRight from 'public/assets/svg/arrow.svg';
 
 interface MilestoneLinkProps {
   roadmapSlug: string;
   code: string;
 }
 
-const MilestoneLink: React.FC<MilestoneLinkProps> = ({ roadmapSlug, code }) => {
-  const theme = useTheme();
-
-  return (
-    <LinkCard href={`${siteRoutes.roadmapMilestones(roadmapSlug)}#${code}`}>
-      <TextBox>
-        <Milestone>Milestone</Milestone>
-        <Code>{code}</Code>
-      </TextBox>
-      <ArrowContainer>
-        <ArrowNavigationForCards width={24} height={24} fill={theme.palette.colors.sky[1000]} />
-      </ArrowContainer>
-    </LinkCard>
-  );
-};
+const MilestoneLink: React.FC<MilestoneLinkProps> = ({ roadmapSlug, code }) => (
+  <LinkCard href={`${siteRoutes.roadmapMilestones(roadmapSlug)}#${code}`}>
+    <TextBox>
+      <Milestone>Milestone</Milestone>
+      <Code>{code}</Code>
+    </TextBox>
+    <ArrowContainer>
+      <Arrow width={24} height={24} />
+    </ArrowContainer>
+  </LinkCard>
+);
 
 export default MilestoneLink;
 
@@ -31,33 +27,30 @@ const LinkCard = styled(Link)(({ theme }) => ({
   justifyContent: 'space-between',
   width: '100%',
   overflow: 'hidden',
-  borderRadius: 6,
-  background: theme.palette.isLight ? '#fff' : '#31424E',
-  boxShadow: theme.palette.isLight
-    ? '0px 1px 3px 0px rgba(190, 190, 190, 0.25), 0px 5px 10px 0px rgba(219, 227, 237, 0.40)'
-    : '10px 0px 20px 6px rgba(20, 0, 141, 0.10)',
+  borderRadius: 8,
+  background: theme.palette.isLight ? '#fff' : theme.palette.colors.charcoal[800],
+  boxShadow: theme.palette.isLight ? theme.fusionShadows.chartsShadows : 'none',
 }));
 
 const TextBox = styled('div')({
   display: 'flex',
   alignItems: 'center',
   gap: 8,
-  padding: 8,
+  padding: '4px 8px',
 });
 
 const Milestone = styled('div')(({ theme }) => ({
   position: 'relative',
-  color: theme.palette.isLight ? '#B6BCC2' : '#787A9B',
+  color: theme.palette.isLight ? theme.palette.colors.slate[100] : theme.palette.colors.slate[200],
   fontSize: 12,
-  fontWeight: 600,
-  lineHeight: 'normal',
-  letterSpacing: 1,
+  fontWeight: 500,
+  lineHeight: '18px',
   textTransform: 'uppercase',
   paddingRight: 9,
 
   '&:after': {
     content: '""',
-    background: theme.palette.isLight ? '#D4D9E1' : '#787A9B',
+    background: theme.palette.isLight ? theme.palette.colors.charcoal[100] : theme.palette.colors.slate[200],
     position: 'absolute',
     right: 0,
     top: 0,
@@ -67,18 +60,28 @@ const Milestone = styled('div')(({ theme }) => ({
 }));
 
 const Code = styled('div')(({ theme }) => ({
-  color: theme.palette.isLight ? '#434358' : '#787A9B',
+  color: theme.palette.isLight ? theme.palette.colors.slate[100] : theme.palette.colors.slate[200],
   fontSize: 14,
   fontWeight: 600,
-  lineHeight: 'normal',
+  lineHeight: '22px',
 }));
 
 const ArrowContainer = styled('div')(({ theme }) => ({
   display: 'flex',
-  backgroundColor: theme.palette.isLight ? 'rgba(236, 239, 249, 0.50)' : 'rgba(124, 107, 149, 0.30)',
+  backgroundColor: theme.palette.isLight ? theme.palette.colors.slate[50] : theme.palette.colors.charcoal[700],
   alignItems: 'center',
   overflow: 'hidden',
   justifyContent: 'center',
   width: 32,
-  height: 34,
+  height: 32,
+}));
+
+const Arrow = styled(ArrowRight)(({ theme }) => ({
+  '& path': {
+    fill: theme.palette.isLight ? theme.palette.colors.slate[100] : theme.palette.colors.charcoal[300],
+  },
+
+  '&:hover path': {
+    fill: theme.palette.colors.sky[1000],
+  },
 }));
