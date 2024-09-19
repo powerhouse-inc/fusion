93720d68d30bb2da2608e93aa1529ae2dd20919b
Yasiel Cabrera <yasiel9506@gmail.com>
2024-09-18 21:53:21 -0400

    fix: disable sitemap through feature flags

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,15 @@
+import { featureFlags } from 'feature-flags/feature-flags';
+import { notFound } from 'next/navigation';
+import { CURRENT_ENVIRONMENT } from '@/config/endpoints';
 import SitemapBuilder from '@/core/utils/sitemap';
 import type { MetadataRoute } from 'next';
 
 const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
+  const flags = featureFlags[CURRENT_ENVIRONMENT];
+  if (!flags.FEATURE_SITEMAP) {
+    return notFound();
+  }
+
   const builder = new SitemapBuilder();
   return await builder.build();
 };
