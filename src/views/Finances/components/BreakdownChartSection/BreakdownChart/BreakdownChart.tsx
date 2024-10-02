3fd0d37c90bccbee790ca3fcb3c3367d9a157111
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2024-10-02 10:21:13 -0300

    Fix line in table

--- src/views/Finances/components/BreakdownChartSection/BreakdownChart/BreakdownChart.tsx
+++ src/views/Finances/components/BreakdownChartSection/BreakdownChart/BreakdownChart.tsx
@@ -56,7 +56,8 @@ const BreakdownChart: React.FC<BreakdownChartProps> = ({
 
   const isMobileOrLess = isMobile || isLessMobile;
   const showLineYear =
-    ((isMobile || isLessMobile) && selectedGranularity === 'monthly') || selectedGranularity === 'quarterly';
+    (isMobile || isLessMobile) && (selectedGranularity === 'monthly' || selectedGranularity === 'quarterly');
+
   // Values for the grid
   const getHeightGrid = useCallback(() => {
     switch (true) {
